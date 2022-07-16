71f47d51ea8c0cb425f76b059d0f63f7e2adc62d
oz <osazeowie0@gmail.com>
2022-07-16 01:24:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const namee = prompt("Enter name:");
+const height = prompt("Enter height:");
+const country = prompt("Enter country:");
+
+alert(`Name: ${namee}`);
+alert(`height: ${height}`);
+alert(`country: ${country}`);
